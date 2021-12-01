226a7fdef5752d25a2e164ed6e7205b3d3f2f9a8
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-01 17:09:45 +0200

    stack with positioned background image & text

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -5,6 +5,30 @@ class StackWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Image.asset('assets/images/tree.jpg'),
+        const Positioned(
+          left: 10.0,
+          bottom: 10.0,
+          child: CircleAvatar(
+            radius: 48.0,
+            backgroundImage: AssetImage('assets/images/lion.jpg'),
+          ),
+        ),
+        const Positioned(
+          left: 26.0,
+          bottom: 16.0,
+          child: Text(
+            'Lion',
+            style: TextStyle(
+              fontSize: 32.0,
+              color: Colors.white30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
