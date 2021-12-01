7bc4a2704e15350a9f6678eaa99ac603a59041ee
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-01 17:22:58 +0200

    implement stack-favorite widget

--- lib/widgets/stack_favorite.dart
+++ lib/widgets/stack_favorite.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class StackFavoriteWidget extends StatelessWidget {
@@ -5,6 +7,53 @@ class StackFavoriteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.black87,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Stack(
+          children: [
+            Image.asset('assets/images/dawn.jpg'),
+            const Positioned(
+              top: 0.0,
+              right: 0.0,
+              child: FractionalTranslation(
+                translation: Offset(0.3, -0.3),
+                // translation: Offset(0.0, -0.0),
+                child: CircleAvatar(
+                  radius: 24.0,
+                  backgroundColor: Colors.white30,
+                  child: Icon(
+                    Icons.star,
+                    size: 24.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: CircleAvatar(
+                radius: 48.0,
+                backgroundImage: AssetImage('assets/images/eagle.jpg'),
+              ),
+            ),
+            const Positioned(
+              bottom: 16.0,
+              left: 16.0,
+              child: Text(
+                'Bald Eagle',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.white30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
